0823bde17aacacbcc6c94509270bae54444785eb
turueee <turushkine@inbox.ru>
2024-08-20 18:46:38 +0300

    Изменение ссылок

--- data.py
+++ data.py
@@ -14,33 +14,34 @@ def mexadatas():
 
 
 def anistudio(studioname):
-    id = dataconecter(f"SELECT id_anime FROM about_anime WHERE rusname = '{studioname}'").fetchone()[0]
+    id = dataconecter(f"SELECT id_anime FROM about_anime WHERE engname = '{studioname}'").fetchone()[0]
     studio = dataconecter(f"SELECT studio FROM studio WHERE id_anime={id}").fetchall()
     studios = [i[0] for i in studio]
     return studios
 
 
+
 def aniinfo(studioname):
     info = dataconecter(
-        f"SELECT director,ruslic, oppening, logo FROM about_anime WHERE rusname = '{studioname}'"
+        f"SELECT director,ruslic, oppening, logo FROM about_anime WHERE engname = '{studioname}'"
     ).fetchone()
     placeholders = ['rezh', 'lic', 'music', 'logo']
     return (dict(zip(placeholders, info)))
 
 
 def infcharacters(studioname):
-    id = dataconecter(f"SELECT id_anime FROM about_anime WHERE rusname = '{studioname}'").fetchone()[0]
+    id = dataconecter(f"SELECT id_anime FROM about_anime WHERE engname = '{studioname}'").fetchone()[0]
     characters = dataconecter(f"SELECT name,information FROM character WHERE id_anime={id}").fetchall()
     return (dict(characters))
 
 
 def photocharacters(studioname):
-    id = dataconecter(f"SELECT id_anime FROM about_anime WHERE rusname = '{studioname}'").fetchone()[0]
+    id = dataconecter(f"SELECT id_anime FROM about_anime WHERE engname = '{studioname}'").fetchone()[0]
     photocharacter = dataconecter(f"SELECT name,photo FROM character WHERE id_anime={id}").fetchall()
     return (dict(photocharacter))
 
 
 def groopphoto(studioname):
-    groop = dataconecter(f"SELECT engname FROM about_anime WHERE rusname = '{studioname}'").fetchone()[0]
+    groop = dataconecter(f"SELECT rusname FROM about_anime WHERE engname = '{studioname}'").fetchone()[0]
     return(groop)
 
